8a0e05d6fc3d6ad3c3e082491aee74bf8a703719
Bernice Kipkeny <bcoderock@gmail.com>
2024-05-07 02:40:48 -0400

    call function

--- main.py
+++ main.py
@@ -5,5 +5,14 @@ def deposit():
         amount =input("What would you like to deposit? $")
         if amount.isdigit():
             amount =int(amount)
-        
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Please enter a number.")
+
+    return amount
+
+deposit()
     
