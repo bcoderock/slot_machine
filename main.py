babc583dd57013b77a739426e80706d5ac2174d0
Bernice Kipkeny <bcoderock@gmail.com>
2024-05-07 02:30:16 -0400

    while True

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 # this is a text based slot machine details in notes.txt
 
+def deposit():
+    while True :
+        amount =input("What would you like to deposit? $")
+        if amount.isdigit():
+            amount =int(amount)
+        
+    
