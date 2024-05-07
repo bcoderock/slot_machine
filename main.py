60e7f73092d1477d6e083ec56c5fea6479136b3d
Bernice Kipkeny <bcoderock@gmail.com>
2024-05-07 03:23:03 -0400

    def main

--- main.py
+++ main.py
@@ -25,15 +25,17 @@ def get_number_of_lines():
             if 1 <= lines <= MAX_LINES:
                 break
             else:
-                print("Amount must be greater than 0.")
+                print("Enter a valid number of lines.")
         else:
             print("Please enter a number.")
 
-        return amount
+        return lines
    
 
 def main():
     balance=deposit()
+    lines=get_number_of_lines()
+    print(balance,lines)
 
 main()
 
