b5848091dcf442c1313174bb140909cb57427efe
Bernice Kipkeny <bcoderock@gmail.com>
2024-05-07 04:21:48 -0400

    get_bet()

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # this is a text based slot machine details in notes.txt
 #adding a global constant. CONSTANTS ARE IN ALL CAPS
 MAX_LINES = 3 
+MAX_BET = 100
+MIN_BET = 1
 def deposit():
     while True :
         amount =input("What would you like to deposit? $")
@@ -30,7 +32,21 @@ def get_number_of_lines():
             print("Please enter a number.")
 
      return lines
-   
+
+def get_bet():
+    while True :
+        amount =input("What would you like to bet? $")
+        if amount.isdigit():
+            amount =int(amount)
+            if MIN_BET <= amount <= MAX_BET :
+                break
+            else:
+                print( f"Amount must be between{MIN_BET}-{MAX_BET}.")
+        else:
+            print("Please enter a number.")
+
+    return amount    
+      
 
 def main():
     balance=deposit()
